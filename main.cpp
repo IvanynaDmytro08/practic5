6126704525c6bc34998f2b626c1eef3b1537dd0f
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-11-10 19:01:41 +0200

    All work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
+class Overcoat{
+
+    int m_price;
+    int m_typeClothes;
+
+public:
+    Overcoat(int price = 400, int type = 2):m_price(price), m_typeClothes(type){}
+
+    Overcoat& operator = (Overcoat& O1){
+        m_price = O1.m_price;
+        m_typeClothes = O1.m_typeClothes;
+        return *this;
+    }
+
+    friend bool operator ==(Overcoat& o1, Overcoat& o2);
+    friend bool operator>(Overcoat& o1, Overcoat o2 );
+
+};
+
+ bool operator == (Overcoat& o1, Overcoat& o2){
+    return (o1.m_typeClothes == o2.m_typeClothes);
+}
+
+bool operator>(Overcoat& o1, Overcoat o2) {
+    return o1.m_price > o2.m_price;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+
+    Overcoat object1(300, 2);
+    Overcoat objeckt2(4000, 1);
+
+    std::cout << "Type clothes - " << (object1 == objeckt2) << std::endl;
+    std::cout << "Price 1) - " << (object1 > objeckt2) << std::endl;
+    std::cout << "Price 2) - " << (objeckt2 > object1) << std::endl;
+
+    object1 = objeckt2;
+
+    std::cout << "Type clothes - " << (object1 == objeckt2) << std::endl;
+    std::cout << "Price 1) - " << (object1 > objeckt2) << std::endl;
+    std::cout << "Price 2) - " << (objeckt2 > object1) << std::endl;
+
+
+
+
+
+
 }
