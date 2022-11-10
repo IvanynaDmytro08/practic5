c16aa9b3891d2e6b72ebeaad9155c179f196aaa2
Ivanyna Dmytro <dimaivanina@MacBook-Pro-Dima.local>
2022-11-10 13:43:24 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/practic5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practic5.dir/main.cpp.o"
+  "practic5"
+  "practic5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practic5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
